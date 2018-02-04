52fe844b7f7fa14c5622e8350ca38c442a01c8c6
Gene <gene@coursera-blah.com>
2018-02-04 12:39:41 -0800

    added app.js

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 (function() {
   'use strict';
 
-   angular.module('myFirstApp', [])
-      .controller('MyFirstController', function() {
+   angular.module('NameCalculator', [])
+       .controller('NameCalculatorController', function($scope) {
+          $scope.name="";
+          $scope.totalValue = 0;
 
-    });
+          $scope.displayNumeric = function() {
+             let totalNameValue =
+                    calculateNumericForString($scope.name);
+             $scope.totalValue = totalNameValue;
+          };
+
+          function calculateNumericForString(s) {
+              let totalStringValue = 0;
+              for (let i = 0; i < s.length; i++) {
+                 totalStringValue += s.charCodeAt(i);
+              }
+              return totalStringValue;
+          }
+       });
 })();
